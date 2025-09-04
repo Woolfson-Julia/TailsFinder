3be2f27ffb4bd245c73ce9eb749f62d8164abdf0
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-09-04 17:55:04 +0300

    add mobile styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,67 +32,98 @@ export default function Footer() {
         </a>
         <h2>Контакти</h2>
         <address>
-          <p>
+          <p className={css.contactItem}>
+            <svg
+              className={css.iconContact}
+              width="24"
+              height="24"
+              aria-label="icon Person"
+            >
+              <use href="/sprite.svg#icon-person"></use>
+            </svg>
             <a href="mailto:support@petsafe.ua">support@tailsfinder.ua</a>
           </p>
-          <p>м. Київ, вул. Добрих Сердець, 12</p>
-          <p>
+          <p className={css.contactItem}>
+            <svg
+              className={css.iconLocation}
+              width="24"
+              height="24"
+              aria-label="icon Location"
+            >
+              <use href="/sprite.svg#icon-location"></use>
+            </svg>
+            м. Київ, вул. Добрих Сердець, 12
+          </p>
+          <p className={css.contactItem}>
+            <svg
+              className={css.iconContact}
+              width="24"
+              height="24"
+              aria-label="icon Call"
+            >
+              <use href="/sprite.svg#icon-call"></use>
+            </svg>
             <a href="tel:+3800671234567">+38 (067) 123-45-67</a>
           </p>
         </address>
-        <AddToHomeScreenModal />
         <div className={css.containerNavMedia}>
-          <p className={css.titleNav}>Швидка навігація</p>
-          <nav className={css.nav}>
-            <NavLink to="/" className={buildLinkClass}>
-              Про нас
-            </NavLink>
-            <NavLink to="ads" className={buildLinkClass}>
-              Їх шукають
-            </NavLink>
-          </nav>
-          <p className={css.titleMedia}>Соціальні мережі:</p>
-          <div className={css.containerSocialMedia}>
-            <Link to="https://www.instagram.com">
-              <svg
-                className={css.iconInstagram}
-                width="24"
-                height="24"
-                aria-label="icon Instagram"
-              >
-                <use href="/sprite.svg#icon-instagram"></use>
-              </svg>
-            </Link>
-            <Link to="https://www.facebook.com">
-              <svg
-                className={css.icon}
-                width="24"
-                height="24"
-                aria-label="icon Facebook"
-              >
-                <use href="/sprite.svg#icon-facebook"></use>
-              </svg>
-            </Link>
-            <Link to="https://www.youtube.com">
-              <svg
-                className={css.icon}
-                width="24"
-                height="24"
-                aria-label="icon Youtube"
-              >
-                <use href="/sprite.svg#icon-youtube"></use>
-              </svg>
-            </Link>
-            <Link to="https://www.telegram.org">
-              <svg
-                className={css.icon}
-                width="24"
-                height="24"
-                aria-label="icon Telegram"
-              >
-                <use href="/sprite.svg#icon-telegram"></use>
-              </svg>
-            </Link>
+          <div className={css.containerNavigation}>
+            <p className={css.titleNav}>Швидка навігація</p>
+            <nav className={css.nav}>
+              <NavLink to="/" className={buildLinkClass}>
+                Про нас
+              </NavLink>
+              <NavLink to="ads" className={buildLinkClass}>
+                Їх шукають
+              </NavLink>
+
+              <AddToHomeScreenModal />
+            </nav>
+          </div>
+          <div className={css.containerSocial}>
+            <p className={css.titleMedia}>Соціальні мережі:</p>
+            <div className={css.containerSocialMedia}>
+              <Link to="https://www.instagram.com">
+                <svg
+                  className={css.iconInstagram}
+                  width="24"
+                  height="24"
+                  aria-label="icon Instagram"
+                >
+                  <use href="/sprite.svg#icon-instagram"></use>
+                </svg>
+              </Link>
+              <Link to="https://www.facebook.com">
+                <svg
+                  className={css.icon}
+                  width="24"
+                  height="24"
+                  aria-label="icon Facebook"
+                >
+                  <use href="/sprite.svg#icon-facebook"></use>
+                </svg>
+              </Link>
+              <Link to="https://www.youtube.com">
+                <svg
+                  className={css.icon}
+                  width="24"
+                  height="24"
+                  aria-label="icon Youtube"
+                >
+                  <use href="/sprite.svg#icon-youtube"></use>
+                </svg>
+              </Link>
+              <Link to="https://www.telegram.org">
+                <svg
+                  className={css.icon}
+                  width="24"
+                  height="24"
+                  aria-label="icon Telegram"
+                >
+                  <use href="/sprite.svg#icon-telegram"></use>
+                </svg>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
