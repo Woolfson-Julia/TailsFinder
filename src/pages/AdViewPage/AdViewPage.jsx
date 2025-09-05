dc2528bbbf28682f2f0071fe443298306dd5fdc3
Tetyana Solyanyk <tanyas81@gmail.com>
2025-09-05 07:31:24 +0300

    style: commented-out code in AdViewPage

--- src/pages/AdViewPage/AdViewPage.jsx
+++ src/pages/AdViewPage/AdViewPage.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 // import {selectRecipesError, selectRecipesLoading } from '../../redux/recipes/selectors';
-import { fetchRecipeById } from '../../redux/recipes/operations';
+// import { fetchRecipeById } from '../../redux/recipes/operations';
 
 // import NotFound from '../../components/NotFound/NotFound';
-import AdDetails from '../../components/AdDetails/AdDetails';
+import AdDetails from "../../components/AdDetails/AdDetails";
 // import RecipeDetails from '../../components/RecipeDetails/RecipeDetails';
 // import Loader from '../../components/Loader/Loader';
-import css from './AdViewPage.module.css'
+import css from "./AdViewPage.module.css";
 
 export default function AdViewPage() {
   const { id } = useParams();
@@ -18,15 +18,14 @@ export default function AdViewPage() {
   // const isLoading = useSelector(selectRecipesLoading);
 
   useEffect(() => {
-    
-    dispatch(fetchRecipeById(id));
+    // dispatch(fetchRecipeById(id));
   }, [id, dispatch]);
   return (
     <>
       {/* {isLoading && <Loader />}
       {error && <NotFound />} */}
       {/* {recipe && <RecipeDetails recipe={recipe} />} */}
-      <AdDetails/>
+      <AdDetails />
     </>
   );
 }
