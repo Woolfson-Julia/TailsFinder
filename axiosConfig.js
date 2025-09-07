d45a720dad47d90b1d31992d9074b4e90b2fac7a
Woolf <woolfson1995@gmail.com>
2025-09-07 19:33:30 +0300

    changed the linl to the backend

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://tails-finderbd.onrender.com/api",
+  // baseURL: "https://tails-finderbd.onrender.com/api",
+  baseURL: "https://tails-finderbd-production.up.railway.app/api",
 });
 
 export default instance;
